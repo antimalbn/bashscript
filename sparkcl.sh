8998355ec598b5c458983484bc5d37be06eaa2a8
EC2 Default User <ec2-user@ip-172-31-38-255.ec2.internal>
2019-10-09 13:49:35 +0000

    added new script version

--- sparkcl.sh
+++ sparkcl.sh
@@ -15,7 +15,7 @@ if [ ! -z "$ppv" ] ;
    then
       pip3 install --user flintrock 
       pip3 install --user "cryptography==2.4.2" 
-
+      createdefaultvalue 
 else
      echo "Unbale to install pip3"
      exit
@@ -24,14 +24,24 @@ fi
 }
 
 #Create new cluster
-createcl(){
 
-dat=$(date +"%Y%m%d%H%M")
-sgname=sparkcllanaccess$dat
+createdefaultvalue(){
+sgname=sparkcllanaccess
+checksg=`aws ec2 describe-security-groups --filter Name=group-name,Values=sparkcllanaccess | grep GroupId | awk {'print $2'} | tr -d '" ' | tr -d ", "`
+if [ ! -z "$checksg" ] ;
+    then 
+         sgid=$checksg
+    else 	 
+
 vpccidr=`aws ec2 describe-vpcs  | grep CidrBlock | grep -v "CidrBlockAssociationSet" | grep -v "CidrBlockState" | head -1 | awk {'print $2'} | tr -d '" ' | tr -d ", "`
 vpcid=`aws ec2 describe-vpcs  | grep VpcId | awk {'print $2'} | tr -d '" ' | tr -d ", "`
 
-sgid=`aws ec2 create-security-group --group-name $sgname --description "My security group" --vpc-id $vpcid | grep sg | awk {'print $2'} | tr -d '" ' | tr -d ", "`
+sgid=`aws ec2 create-security-group --group-name $sgname --description "Spark Cluster SG" --vpc-id $vpcid | grep sg | awk {'print $2'} | tr -d '" ' | tr -d ", "`
+
+fi
+
+vpccidr=`aws ec2 describe-vpcs  | grep CidrBlock | grep -v "CidrBlockAssociationSet" | grep -v "CidrBlockState" | head -1 | awk {'print $2'} | tr -d '" ' | tr -d ", "`
+
 
 aws ec2 authorize-security-group-ingress \
     --group-id $sgid \
@@ -48,35 +58,51 @@ aws ec2 authorize-security-group-ingress \
     --group-id $sgid \
     --protocol tcp \
     --port 9999 \
-    --cidr 0.0.0.0/0    
-    
+    --cidr 0.0.0.0/0
+
+
+read -p "Enter EC2 Key name " dkeyname
+echo $dkeyname  > clkey.txt
+
+read -p "Enter AMI ID " damiclid
+echo $damiclid > clamid.txt
+
+
+read -p "Enter Instance profile name " dimrol
+echo $dimrol > cliam.txt
+
+
+}
+
+
+
+createcl(){
+
 
 read -p "Enter Cluster Name  " clname
 read -p "Enter Number of slave to launch " slvnum
-read -p "Enter EC2 Key name " keyname
-read -p "Enter EC2 key path like /spark/mysrv.pem  " keypath
-read -p "Enter AMI ID " amiclid
 read -p "Enter Instance type like t2.micro " instancetype
-read -p "Enter Instance profile name " imrol
+
+cldkeyname=`cat clkey.txt`
+cldimrol=`cat cliam.txt`
+cldamiclid=`cat clamid.txt`
+
 
 flintrock launch $clname \
     --num-slaves $slvnum \
     --spark-version 2.4.4 \
-    --ec2-key-name $keyname \
-    --ec2-identity-file $keypath \
-    --ec2-ami $amiclid \
+    --ec2-key-name $cldkeyname \
+    --ec2-identity-file $cldkeyname.pem \
+    --ec2-ami $cldamiclid \
     --ec2-user ec2-user \
     --install-hdfs \
     --hdfs-version 2.8.5 \
     --install-spark \
     --ec2-instance-type $instancetype \
-    --ec2-instance-profile-name $imrol \
-    --ec2-security-group $sgname
+    --ec2-instance-profile-name $cldimrol \
+    --ec2-security-group sparkcllanaccess
 
 
-#flintrock run-command --ec2-identity-file $keypath --ec2-user ec2-user $clname "/bin/bash /home/ec2-user/copys3lib.sh"
-flintrock copy-file --master-only --ec2-identity-file $keypath --ec2-user ec2-user $clname notebook.sh /home/ec2-user/
-flintrock run-command --master-only --ec2-identity-file $keypath --ec2-user ec2-user $clname "/bin/bash notebook.sh"
 exit 
 
 }
@@ -96,10 +122,11 @@ addslave(){
 
 read -p "Enter Cluster Name  " clname
 read -p "Enter Number of slave to add  " slvnum
-read -p "Enter EC2 key file path " keypath
+cldkeyname=`cat clkey.txt`
 
-flintrock add-slaves --ec2-identity-file $keypath --ec2-user ec2-user $clname --num-slaves=$slvnum
-flintrock run-command --ec2-identity-file $keypath --ec2-user ec2-user $clname "/bin/bash /home/ec2-user/copys3lib.sh"
+
+flintrock add-slaves --ec2-identity-file $cldkeyname.pem --ec2-user ec2-user $clname --num-slaves=$slvnum
+flintrock run-command --ec2-identity-file $cldkeyname.pem --ec2-user ec2-user $clname "/bin/bash /home/ec2-user/copys3lib.sh"
 exit
 
 }
@@ -109,9 +136,10 @@ removeslave(){
 
 read -p "Enter Cluster Name  " clname
 read -p "Enter Number of slave to remove  " slvnum
-read -p "Enter EC2 key path like /spark/mysrv.pem  " keypath
+cldkeyname=`cat clkey.txt`
 
-flintrock remove-slaves --ec2-identity-file $keypath --ec2-user ec2-user $clname --num-slaves=$slvnum
+
+flintrock remove-slaves --ec2-identity-file $cldkeyname.pem --ec2-user ec2-user $clname --num-slaves=$slvnum
 
 exit
 
@@ -121,9 +149,10 @@ exit
 startcl(){
 
 read -p "Enter Cluster Name  " clname
-read -p "Enter EC2 key path like /spark/mysrv.pem  " keypath
-	
-flintrock start --ec2-identity-file $keypath --ec2-user ec2-user  $clname
+cldkeyname=`cat clkey.txt`
+
+
+flintrock start --ec2-identity-file $cldkeyname.pem --ec2-user ec2-user  $clname
 
 exit
 
@@ -133,6 +162,8 @@ exit
 stopcl(){
 
 read -p "Enter Cluster Name  " clname
+cldkeyname=`cat clkey.txt`
+
 
 flintrock stop $clname	
 
@@ -145,10 +176,42 @@ exit
 getcldetails(){
 
 read -p "Enter Cluster Name  " clname
+cldkeyname=`cat clkey.txt`
+
 
 flintrock describe $clname	
 
 exit
+
+}
+
+
+#Login to cluster
+
+logincl(){
+
+read -p "Enter Cluster Name  " clname
+cldkeyname=`cat clkey.txt`
+
+
+flintrock login --ec2-identity-file $cldkeyname.pem --ec2-user ec2-user  $clname
+
+exit
+
+
+}	
+
+runcmdcl(){
+
+read -p "Enter Cluster Name  " clname
+
+read -p "Enter comand to run  " cmdtorun
+cldkeyname=`cat clkey.txt`
+
+flintrock run-command --ec2-identity-file $cldkeyname.pem --ec2-user ec2-user  $clname $cmdtorun
+
+exit
+
 
 }	
 
@@ -195,7 +258,9 @@ show_menusexclmg(){
 	echo "4. Stop cluser"
 	echo "5. Start cluser"
 	echo "6. Destroy cluser"
-        echo "7. Exit"
+        echo "7. Login to cluser"
+	echo "8. Run command to cluser"
+        echo "9. Exit"
 
 
 }
@@ -249,7 +314,9 @@ read_exclmgoptions(){
 		4) stopcl ;;
 		5) startcl ;;
 		6) destroycl ;;
-                7) exit 0 ;;
+		7) logincl ;;
+		8) runcmdcl ;;
+                9) exit 0 ;;
                 *) echo -e "${RED}Error...${STD}" && sleep 2
         esac
 }
